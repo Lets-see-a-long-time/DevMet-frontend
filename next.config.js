54ae45075b0d1f1f693eaebb0cc7b9e5b8c52d85
김인태 <dlsxody1@naver.com>
2023-09-13 19:16:40 +0900

    TODO : editor onchange, api "

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     appDir: true,
   },
   typescript: {},
+  images: {
+    domains: ["s3://devmet-images/devmet/.s3.ap-northeast-2.amazonaws.com"],
+  },
 };
 
 module.exports = nextConfig;
